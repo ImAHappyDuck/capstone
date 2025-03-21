2ac3c32fe3dc5931ef46f273f6eae6cc33caf32e
StineAC21 <100294758+StineAC21@users.noreply.github.com>
2025-03-21 10:35:24 -0400

    modelsA

---
+++ modelsA.py
@@ -0,0 +1,119 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error, r2_score, accuracy_score, confusion_matrix, classification_report, mean_absolute_error
+import joblib
+import argparse
+import warnings
+warnings.filterwarnings("ignore")
+from sklearn.ensemble import RandomForestClassifier
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", choices=["a", "b", "c"], required=True, help="a, b, or c?")
+args = parser.parse_args()
+
+if args.model == "a":
+    #Model a, Logistic regression predicting ITM or OTM using old columns
+    df = pd.read_csv('cleaned_optData.csv')
+    data = df[df['moneyness'].isna() == False]
+    data['num_position'] = data['position'].apply(lambda x: 1 if x == 'ITM' else 0)
+
+    X = data[['bid', 'delta', 'gamma', 'theta', 'vega', 'rho']]
+    y = data['num_position']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    model = LogisticRegression()
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    print(f'Mean Squared Error: {mse}')
+    print(f'R^2 Score: {r2}')
+
+    accuracy = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    class_report = classification_report(y_test, y_pred)
+
+    print(f"Accuracy: {accuracy:.4f}")
+    print("\nConfusion Matrix:")
+    print(conf_matrix)
+    print("\nClassification Report:")
+    print(class_report)
+
+#Model b, logistic regression using the new columns
+elif args.model == "b":
+
+    df = pd.read_csv('cleaned_optData.csv')
+    data = df[df['moneyness'].isna() == False]
+    data['num_position'] = data['position'].apply(lambda x: 1 if x == 'ITM' else 0)
+
+    X = data[['bid', 'ask','rho']]
+    y = data['num_position']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    model = LogisticRegression()
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    print(f'Mean Squared Error: {mse}')
+    print(f'R^2 Score: {r2}')
+
+    accuracy = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    class_report = classification_report(y_test, y_pred)
+
+    print(f"Accuracy: {accuracy:.4f}")
+    print("\nConfusion Matrix:")
+    print(conf_matrix)
+    print("\nClassification Report:")
+    print(class_report)
+
+# model c, random forest classifier using the columns found in feature selection
+elif args.model == "c":
+    df = pd.read_csv('cleaned_optData.csv')
+    data = df[df['moneyness'].isna() == False]
+    data['num_position'] = data['position'].apply(lambda x: 1 if x == 'ITM' else 0)
+
+    X = data[['bid', 'ask','rho']]
+    y = data['num_position']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    model = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+
+    print(f'Mean Squared Error: {mse}')
+    print(f'R^2 Score: {r2}')
+
+    accuracy = accuracy_score(y_test, y_pred)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    class_report = classification_report(y_test, y_pred)
+
+    print(f"Accuracy: {accuracy:.4f}")
+    print("\nConfusion Matrix:")
+    print(conf_matrix)
+    print("\nClassification Report:")
+    print(class_report)
+
+    
+ 
+
+    
