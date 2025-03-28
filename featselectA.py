f83477187181dff933766055ec34608c8f9ec7a6
StineAC21 <100294758+StineAC21@users.noreply.github.com>
2025-03-28 10:40:05 -0400

    crap with profit

--- featselectA.py
+++ featselectA.py
@@ -1,29 +1,63 @@
 import pandas as pd
 import numpy as np
+import argparse
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, f_regression
 
-# Load dataset
-data = pd.read_csv('cleaned_optData.csv')
-data = data.dropna()
-numeric_cols = data.select_dtypes(include=[np.number]).columns.tolist()
-numeric_cols.remove('moneyness') 
-X = data[numeric_cols]
-y = data.moneyness
+parser = argparse.ArgumentParser()
+parser.add_argument("--featSelect", choices=["old", "new"], required=True, help="models are old or new")
+args = parser.parse_args()
 
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+if args.featSelect == "old":
+    # Load dataset
+    data = pd.read_csv('cleaned_optData.csv')
+    data = data.dropna()
+    numeric_cols = data.select_dtypes(include=[np.number]).columns.tolist()
+    numeric_cols.remove('moneyness') 
+    X = data[numeric_cols]
+    y = data.moneyness
+
+    # Split data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
 
-# selecting best 3 features with f_regression
-select_k_best = SelectKBest(score_func=f_regression, k=3)
-X_train_k_best = select_k_best.fit_transform(X_train, y_train)
+    # selecting best 3 features with f_regression
+    select_k_best = SelectKBest(score_func=f_regression, k=3)
+    X_train_k_best = select_k_best.fit_transform(X_train, y_train)
 
-# selecting the 3 best features to keep and gets the scores for the features
-selected_features = X_train.columns[select_k_best.get_support()]
-feature_scores = select_k_best.scores_[select_k_best.get_support()]
+    # selecting the 3 best features to keep and gets the scores for the features
+    selected_features = X_train.columns[select_k_best.get_support()]
+    feature_scores = select_k_best.scores_[select_k_best.get_support()]
 
-# print the selected features and then their scores
-print("Selected features:", X_train.columns[select_k_best.get_support()])
-for feature, score in zip(selected_features, feature_scores):
-    print(f"Feature: {feature}, Score: {score}")
+    # print the selected features and then their scores
+    print("Selected features:", X_train.columns[select_k_best.get_support()])
+    for feature, score in zip(selected_features, feature_scores):
+        print(f"Feature: {feature}, Score: {score}")
+
+elif args.featSelect == "new":
+    # Load dataset
+    data = pd.read_csv('cleaned_optData_with_prices.csv')
+    data = data.dropna()
+    numeric_cols = data.select_dtypes(include=[np.number]).columns.tolist()
+    numeric_cols.remove('profit')
+    # I'm removing this because I think profit is based off of it
+    numeric_cols.remove('moneyness')
+    X = data[numeric_cols]
+    y = data.profit
+
+    # Split data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+
+
+    # selecting best 3 features with f_regression
+    select_k_best = SelectKBest(score_func=f_regression, k=3)
+    X_train_k_best = select_k_best.fit_transform(X_train, y_train)
+
+    # selecting the 3 best features to keep and gets the scores for the features
+    selected_features = X_train.columns[select_k_best.get_support()]
+    feature_scores = select_k_best.scores_[select_k_best.get_support()]
+
+    # print the selected features and then their scores
+    print("Selected features:", X_train.columns[select_k_best.get_support()])
+    for feature, score in zip(selected_features, feature_scores):
+        print(f"Feature: {feature}, Score: {score}")
